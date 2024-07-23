ea1001d9820abf88fa7ba9f978ffe3111cf4fdfe
luqman Shadow <luqmankyembe17@gmail.com>
2024-07-23 12:21:32 +0300

    final

--- final.py
+++ final.py
@@ -1,162 +1,111 @@
-""" this will create a funciton that asks how many layers the user wants.
-it will then create a circle of shapes starting with a triangle and 
-increasing one side for each new layer"""
+import turtle
 
-#sets the speed to 0
-speed(0)
+# Setup turtle
+turtle.speed(0)
+turtle.pensize(5)
+turtle.colormode(255)
 
-#puts the pen size to 5
-pensize(5)
+# Ask the user how many layers of art there should be
+layers = int(input("How many layers should there be? (1-6): "))
 
-#asks the user how many layers of art there should be
-layers=int(input("how many layers should there be?(1-6)"))
+# Initial values
+radius = 80
+step = 3
+r, g, b = 255, 0, 0
 
-#sets the initial radius of the first circle to 80
-radius=80
-step=3
-r =255
-g=0
-b=0
-
-#this will as the user which color each layer should be. it also creates the 
-#different layers of shapes
+# Function to draw the shapes in layers
 for i in range(layers):
-    for i in range (37):
-        penup()
-        color((r,g,b))      
-        forward (radius)
-        #tracy moves forard
-        pendown()
-        circle (20,360,step)
-        #draws the circle
-        penup()
-        backward (radius)
-        #back to center
-        left (10)
-    #increases the radius of the next circle 
-    step=step+1
-    radius=radius+20
-    r=r-55
-    g=g+20
-    b=b+25
-    
-#makes tracy face north
-setheading(90)
+    for j in range(36):
+        turtle.penup()
+        turtle.color(r, g, b)
+        turtle.forward(radius)
+        turtle.pendown()
+        turtle.circle(20, 360, step)
+        turtle.penup()
+        turtle.backward(radius)
+        turtle.left(10)
+    step += 1
+    radius += 20
+    r = max(0, r - 55)
+    g = min(255, g + 20)
+    b = min(255, b + 25)
 
-#creates the number 1
+# Draw the number of layers in the center
+turtle.penup()
+turtle.setheading(90)
+turtle.goto(0, -20)
+
+# Functions to draw the numbers
 def one():
-    pendown()
-    forward(30)
-    backward(60)
-    
-#creates the number 2
+    turtle.pendown()
+    turtle.forward(60)
+    turtle.backward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.penup()
+
 def two():
-    pendown()
-    right(90)
-    forward (30)
-    left(90)
-    forward (30)
-    left(90)
-    forward(60)
-    penup()
-    setposition(0,0)
-    pendown()
-    forward(30)
-    left(90)
-    forward(30)
-    left(90)
-    forward(60)
-    
-#creates the number 3
+    turtle.pendown()
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.penup()
+
 def three():
-    pendown()
-    right(90)
-    forward (30)
-    left(90)
-    forward (30)
-    left(90)
-    forward(60)
-    penup()
-    setposition(0,0)
-    pendown()
-    right(180)
-    forward(30)
-    right(90)
-    forward (30)
-    right(90)
-    forward(60)
+    turtle.pendown()
+    for _ in range(2):
+        turtle.forward(30)
+        turtle.backward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.left(90)
+    turtle.forward(30)
+    turtle.penup()
 
-#creates the number 4
 def four():
-    pendown()
-    left(90)
-    forward(20)
-    right(90)
-    forward(30)
-    penup()
-    setposition(0,0)
-    pendown()
-    right(90)
-    forward(20)
-    left(90)
-    forward(30)
-    backward(70)
+    turtle.pendown()
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.backward(60)
+    turtle.right(90)
+    turtle.penup()
 
-#creates the number 5     
 def five():
-    pendown()
-    left(90)
-    forward (30)
-    right(90)
-    forward (30)
-    right(90)
-    forward(60)
-    penup()
-    setposition(0,0)
-    pendown()
-    forward(30)
-    right(90)
-    forward(30)
-    right(90)
-    forward(60)
- 
-#creates the number 6   
+    turtle.pendown()
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.penup()
+
 def six():
-    pendown()
-    left(90)
-    forward (25)
-    right(90)
-    forward (30)
-    right(90)
-    forward(30)
-    penup()
-    setposition(5,0)
-    pendown()
-    left(180)
-    forward (30)
-    left(90)
-    forward (30)
-    left(90)
-    forward(50)
-    penup()
-    setposition(0,0)
-    pendown()
-    forward(20)
-    right(90)
-    forward(30)
-     
-    
-#this will draw the number of layers in the center of the circle based on 
-#how many layers the user wants 
-if layers==1:
+    turtle.pendown()
+    turtle.circle(15, 360, 6)
+    turtle.penup()
+
+# Draw the number of layers
+if layers == 1:
     one()
-elif layers==2:
+elif layers == 2:
     two()
-elif layers==3:
+elif layers == 3:
     three()
-elif layers==4:
+elif layers == 4:
     four()
-elif layers==5:
+elif layers == 5:
     five()
-else:
+elif layers == 6:
     six()
+
+turtle.hideturtle()
+turtle.done()
